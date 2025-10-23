8e5cbd915eb8c3ea27467156e7604fc922ef21d5
ezchapman66 <157661074+ezchapman66@users.noreply.github.com>
2025-10-23 14:13:59 -0500

    this starts the task controller and task routes

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -5,3 +5,35 @@ export function getAllTasks(req, res){
         res.status(500).send("Could not fetch all tasks")
     }
 }
+
+export function getOneTask(req, res){
+    try {
+        res.status(200).send("Got one task")
+    } catch (error) {
+        res.status(500).send("Could not fetch one task")
+    }
+}
+
+export function createTask(req, res){
+    try {
+        res.status(201).send("Created a task")
+    } catch (error) {
+        res.status(500).send("Could not create task")
+    }
+}
+
+export function updateTask(req, res){
+    try {
+        res.status(200).send("Updated Task")
+    } catch (error) {
+        res.status(500).send("Could not update task")
+    }
+}
+
+export function deleteTask(req, res){
+    try {
+        res.status(200).send("Deleted Task")
+    } catch (error) {
+        res.status(500).send("Could not delete task")
+    }
+}
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { getAllTasks } from "../controllers/taskController.js";
+import { createTask, deleteTask, getAllTasks, getOneTask, updateTask } from "../controllers/taskController.js";
 
 const router = express.Router()
 
 router.get("/", getAllTasks);
+router.get("/:id", getOneTask);
+router.post("/", createTask);
+router.put("/:id", updateTask);
+router.delete("/:id", deleteTask);
 
 
 export default router;
